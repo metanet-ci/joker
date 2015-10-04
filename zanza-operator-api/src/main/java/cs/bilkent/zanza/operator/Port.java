a1b4ec49cd2c7e7f653fa78847761159a3490d41
Ensar Basri Kahveci <ebkahveci@gmail.com>
2015-10-04 20:54:37 +0300

    Simplify port definition

--- zanza-operator-api/src/main/java/cs/bilkent/zanza/operator/Port.java
+++ zanza-operator-api/src/main/java/cs/bilkent/zanza/operator/Port.java
@@ -4,35 +4,13 @@ public class Port
 {
 	public static final int DEFAULT_PORT_INDEX = 0;
 
-	public final String sourceOperatorName;
+	public final String operatorName;
 
-	public final int sourcePortIndex;
+	public final int portIndex;
 
-	public final String targetOperatorName;
-
-	public final int targetPortIndex;
-
-	public Port(final String sourceOperatorName, final String targetOperatorName)
+	public Port(String operatorName, int portIndex)
 	{
-		this(sourceOperatorName, DEFAULT_PORT_INDEX, targetOperatorName, DEFAULT_PORT_INDEX);
-	}
-
-	public Port(final String sourceOperatorName, final int sourcePortIndex, final String targetOperatorName,
-			final int targetPortIndex)
-	{
-		this.sourceOperatorName = sourceOperatorName;
-		this.sourcePortIndex = sourcePortIndex;
-		this.targetOperatorName = targetOperatorName;
-		this.targetPortIndex = targetPortIndex;
-	}
-
-	public boolean isSourceDefaultPort()
-	{
-		return sourcePortIndex == DEFAULT_PORT_INDEX;
-	}
-
-	public boolean isTargetDefaultPort()
-	{
-		return targetPortIndex == DEFAULT_PORT_INDEX;
+		this.operatorName = operatorName;
+		this.portIndex = portIndex;
 	}
 }
