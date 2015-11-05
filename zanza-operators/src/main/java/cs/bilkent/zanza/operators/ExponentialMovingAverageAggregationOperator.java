d12b1cf0c17036c840c0fc218afcf065d0805990
Ensar Basri Kahveci <ebkahveci@gmail.com>
2015-11-05 19:48:06 +0300

    Fix engine sub-project's dependencies

---
+++ zanza-operators/src/main/java/cs/bilkent/zanza/operators/ExponentialMovingAverageAggregationOperator.java
@@ -0,0 +1,78 @@
+package cs.bilkent.zanza.operators;
+
+import cs.bilkent.zanza.kvstore.KVStore;
+import cs.bilkent.zanza.operator.InitializationContext;
+import cs.bilkent.zanza.operator.InvocationContext;
+import cs.bilkent.zanza.operator.InvocationResult;
+import cs.bilkent.zanza.operator.Operator;
+import cs.bilkent.zanza.operator.OperatorConfig;
+import cs.bilkent.zanza.operator.OperatorSpec;
+import cs.bilkent.zanza.operator.OperatorType;
+import cs.bilkent.zanza.operator.PortsToTuples;
+import cs.bilkent.zanza.operator.Tuple;
+import cs.bilkent.zanza.scheduling.ScheduleNever;
+import static cs.bilkent.zanza.scheduling.ScheduleWhenTuplesAvailable.scheduleWhenTuplesAvailableOnDefaultPort;
+import cs.bilkent.zanza.scheduling.SchedulingStrategy;
+
+@OperatorSpec( type = OperatorType.STATEFUL, inputPortCount = 1, outputPortCount = 1 )
+public class ExponentialMovingAverageAggregationOperator implements Operator
+{
+
+    public static final String WEIGHT_CONFIG_PARAMETER = "weight";
+
+    public static final String FIELD_NAME_CONFIG_PARAMETER = "fieldName";
+
+    public static final String VALUE_FIELD = "value";
+
+    static final String CURRENT_WINDOW_KEY = "currentWindow";
+
+    static final String TUPLE_COUNT_FIELD = "count";
+
+    private double weight;
+
+    private String fieldName;
+
+    @Override
+    public SchedulingStrategy init ( final InitializationContext context )
+    {
+        final OperatorConfig config = context.getConfig();
+        this.weight = config.getOrFail( WEIGHT_CONFIG_PARAMETER );
+        this.fieldName = config.getOrFail( FIELD_NAME_CONFIG_PARAMETER );
+
+        return scheduleWhenTuplesAvailableOnDefaultPort( 1 );
+    }
+
+    @Override
+    public InvocationResult process ( final InvocationContext invocationContext )
+    {
+        final SchedulingStrategy nextStrategy = invocationContext.isSuccessfulInvocation()
+                                                ? scheduleWhenTuplesAvailableOnDefaultPort( 1 )
+                                                : ScheduleNever.INSTANCE;
+        final PortsToTuples output = new PortsToTuples();
+
+        final KVStore kvStore = invocationContext.getKVStore();
+        final PortsToTuples input = invocationContext.getInputTuples();
+
+        final Tuple currentWindow = kvStore.getOrDefault( CURRENT_WINDOW_KEY, Tuple::new );
+
+        double value = currentWindow.getDoubleValueOrDefault( VALUE_FIELD, 0d );
+        int tupleCount = currentWindow.getIntegerValueOrDefault( TUPLE_COUNT_FIELD, 0 );
+
+        for ( Tuple tuple : input.getTuplesByDefaultPort() )
+        {
+            final double tupleValue = tuple.getDoubleValueOrDefault( fieldName, 0d );
+            value = ( tupleCount++ == 0 ) ? tupleValue : ( weight * tupleValue + ( 1 - weight ) * value );
+            final Tuple avgTuple = new Tuple();
+            avgTuple.set( VALUE_FIELD, value );
+            output.add( avgTuple );
+        }
+
+        currentWindow.set( VALUE_FIELD, value );
+        currentWindow.set( TUPLE_COUNT_FIELD, tupleCount );
+
+        kvStore.set( CURRENT_WINDOW_KEY, currentWindow );
+
+        return new InvocationResult( nextStrategy, output );
+    }
+
+}
---
+++ zanza-operators/src/test/java/cs/bilkent/zanza/operators/ExponentialMovingAverageAggregationOperatorTest.java
@@ -0,0 +1,167 @@
+package cs.bilkent.zanza.operators;
+
+import org.junit.Test;
+
+import static cs.bilkent.zanza.flow.Port.DEFAULT_PORT_INDEX;
+import cs.bilkent.zanza.kvstore.InMemoryKVStore;
+import cs.bilkent.zanza.kvstore.KVStore;
+import cs.bilkent.zanza.operator.InvocationContext;
+import cs.bilkent.zanza.operator.InvocationContext.InvocationReason;
+import cs.bilkent.zanza.operator.InvocationResult;
+import cs.bilkent.zanza.operator.PortsToTuples;
+import cs.bilkent.zanza.operator.Tuple;
+import static cs.bilkent.zanza.operators.ExponentialMovingAverageAggregationOperator.CURRENT_WINDOW_KEY;
+import static cs.bilkent.zanza.operators.ExponentialMovingAverageAggregationOperator.TUPLE_COUNT_FIELD;
+import static cs.bilkent.zanza.operators.ExponentialMovingAverageAggregationOperator.VALUE_FIELD;
+import cs.bilkent.zanza.scheduling.ScheduleWhenTuplesAvailable;
+import static cs.bilkent.zanza.scheduling.ScheduleWhenTuplesAvailable.TupleAvailabilityByCount.AT_LEAST;
+import cs.bilkent.zanza.scheduling.SchedulingStrategy;
+import cs.bilkent.zanza.utils.SimpleInitializationContext;
+import cs.bilkent.zanza.utils.SimpleInvocationContext;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class ExponentialMovingAverageAggregationOperatorTest
+{
+
+    private final ExponentialMovingAverageAggregationOperator operator = new ExponentialMovingAverageAggregationOperator();
+
+    private final SimpleInitializationContext initContext = new SimpleInitializationContext();
+
+    private final PortsToTuples input = new PortsToTuples();
+
+    private final KVStore kvStore = new InMemoryKVStore();
+
+    private final InvocationContext invocationContext = new SimpleInvocationContext( input, InvocationReason.SUCCESS, kvStore );
+
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldFailWithNoTupleCount ()
+    {
+        initContext.getConfig().set( ExponentialMovingAverageAggregationOperator.FIELD_NAME_CONFIG_PARAMETER, "val" );
+        operator.init( initContext );
+    }
+
+    @Test( expected = IllegalArgumentException.class )
+    public void shouldFailWithNoFieldName ()
+    {
+        initContext.getConfig().set( ExponentialMovingAverageAggregationOperator.WEIGHT_CONFIG_PARAMETER, .5 );
+        operator.init( initContext );
+    }
+
+    @Test
+    public void shouldInitializeWithProperConfig ()
+    {
+        setConfig();
+
+        final SchedulingStrategy strategy = operator.init( initContext );
+        assertTrue( strategy instanceof ScheduleWhenTuplesAvailable );
+
+        final ScheduleWhenTuplesAvailable tupleAvailabilitySchedule = (ScheduleWhenTuplesAvailable) strategy;
+        assertThat( tupleAvailabilitySchedule.getTupleCount( DEFAULT_PORT_INDEX ), equalTo( 1 ) );
+        assertThat( tupleAvailabilitySchedule.getTupleAvailabilityByCount(), equalTo( AT_LEAST ) );
+    }
+
+    @Test
+    public void shouldSetAccumulatorForFirstValue ()
+    {
+        setConfig();
+
+        operator.init( initContext );
+        input.add( new Tuple( "val", 10 ) );
+
+        operator.process( invocationContext );
+
+        final Tuple value = kvStore.get( CURRENT_WINDOW_KEY );
+        assertNotNull( value );
+
+        assertValue( value, 10 );
+    }
+
+    @Test
+    public void shouldSetAccumulatorForSecondValue ()
+    {
+        setConfig();
+        setCurrentAvgInKVStore( 0, 5 );
+
+        operator.init( initContext );
+        input.add( new Tuple( "val", 10 ) );
+
+        operator.process( invocationContext );
+
+        final Tuple value = kvStore.get( CURRENT_WINDOW_KEY );
+        assertNotNull( value );
+
+        assertValue( value, 10 );
+    }
+
+    @Test
+    public void shouldReturnFirstAverage ()
+    {
+        setConfig();
+        setCurrentAvgInKVStore( 3, 6 );
+
+        operator.init( initContext );
+        input.add( new Tuple( "val", 4 ) );
+
+        final InvocationResult result = operator.process( invocationContext );
+        final PortsToTuples output = result.getOutputTuples();
+        assertThat( output.getTupleCount( DEFAULT_PORT_INDEX ), equalTo( 1 ) );
+        final Tuple tuple = output.getTuple( DEFAULT_PORT_INDEX, 0 );
+        assertValue( tuple, 5 );
+
+        final Tuple value = kvStore.get( CURRENT_WINDOW_KEY );
+        assertNotNull( value );
+
+        assertValue( value, 5 );
+        assertTupleCount( value, 4 );
+    }
+
+    @Test
+    public void shouldReturnSecondAverage ()
+    {
+        setConfig();
+        setCurrentAvgInKVStore( 4, 10 );
+
+        operator.init( initContext );
+        input.add( new Tuple( "val", 5 ) );
+
+        final InvocationResult result = operator.process( invocationContext );
+        final PortsToTuples output = result.getOutputTuples();
+        assertThat( output.getTupleCount( DEFAULT_PORT_INDEX ), equalTo( 1 ) );
+        final Tuple tuple = output.getTuple( DEFAULT_PORT_INDEX, 0 );
+        assertValue( tuple, 7.5 );
+
+        final Tuple value = kvStore.get( CURRENT_WINDOW_KEY );
+        assertNotNull( value );
+
+        assertValue( value, 7.5 );
+        assertTupleCount( value, 5 );
+    }
+
+    private void setConfig ()
+    {
+        initContext.getConfig().set( ExponentialMovingAverageAggregationOperator.FIELD_NAME_CONFIG_PARAMETER, "val" );
+        initContext.getConfig().set( ExponentialMovingAverageAggregationOperator.WEIGHT_CONFIG_PARAMETER, .5 );
+    }
+
+    private void setCurrentAvgInKVStore ( final int tupleCount, final double value )
+    {
+        final Tuple tuple = new Tuple();
+        tuple.set( TUPLE_COUNT_FIELD, tupleCount );
+        tuple.set( VALUE_FIELD, value );
+        kvStore.set( CURRENT_WINDOW_KEY, tuple );
+    }
+
+    private void assertValue ( final Tuple tuple, final double expectedValue )
+    {
+        assertThat( tuple.getDouble( VALUE_FIELD ), equalTo( expectedValue ) );
+    }
+
+    private void assertTupleCount ( final Tuple tuple, final int expectedTupleCount )
+    {
+        assertThat( tuple.getInteger( TUPLE_COUNT_FIELD ), equalTo( expectedTupleCount ) );
+    }
+
+}
