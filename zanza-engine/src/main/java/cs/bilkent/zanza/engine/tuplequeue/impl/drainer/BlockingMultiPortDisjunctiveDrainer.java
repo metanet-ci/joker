bac63e2ee6f9d5e3b01cc8100a9e792dbf669fff
Ensar Basri Kahveci <ebkahveci@gmail.com>
2016-05-27 16:24:58 +0300

    Reuse inner array in BlockingMultiPortDisjunctiveDrainer

--- zanza-engine/src/main/java/cs/bilkent/zanza/engine/tuplequeue/impl/drainer/BlockingMultiPortDisjunctiveDrainer.java
+++ zanza-engine/src/main/java/cs/bilkent/zanza/engine/tuplequeue/impl/drainer/BlockingMultiPortDisjunctiveDrainer.java
@@ -8,39 +8,42 @@ public class BlockingMultiPortDisjunctiveDrainer extends MultiPortDrainer
 
     private final long timeoutInMillisPerQueue;
 
+    private final int[] tupleCountsBuffer;
+
     public BlockingMultiPortDisjunctiveDrainer ( final int inputPortCount, final long timeoutInMillis )
     {
         super( inputPortCount );
         this.timeoutInMillisPerQueue = (long) Math.ceil( timeoutInMillis / inputPortCount );
+        this.tupleCountsBuffer = new int[ inputPortCount ];
     }
 
     @Override
     protected int[] checkQueueSizes ( final TupleQueue[] tupleQueues )
     {
-        int[] tupleCounts = null;
+        boolean notFound = true;
         for ( int portIndex = 0; portIndex < inputPortCount; portIndex++ )
         {
             final int tupleCount = this.tupleCounts[ portIndex ];
             final TupleQueue tupleQueue = tupleQueues[ portIndex ];
-            if ( tupleCounts == null )
+            if ( notFound )
             {
                 if ( tupleQueue.awaitMinimumSize( tupleCount, timeoutInMillisPerQueue ) )
                 {
-                    tupleCounts = new int[ inputPortCount ];
-                    tupleCounts[ portIndex ] = tupleCount;
+                    tupleCountsBuffer[ portIndex ] = tupleCount;
+                    notFound = false;
                 }
             }
             else if ( tupleQueue.size() >= tupleCount )
             {
-                tupleCounts[ portIndex ] = tupleCount;
+                tupleCountsBuffer[ portIndex ] = tupleCount;
             }
             else
             {
-                tupleCounts[ portIndex ] = NO_TUPLES_AVAILABLE;
+                tupleCountsBuffer[ portIndex ] = NO_TUPLES_AVAILABLE;
             }
         }
 
-        return tupleCounts;
+        return tupleCountsBuffer;
     }
 
 }
