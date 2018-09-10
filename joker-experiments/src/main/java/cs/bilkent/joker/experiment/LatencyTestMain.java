440ce5f7a74a25c1e2dc6994c82318df91a8a39a
Ensar Basri Kahveci <ebkahveci@gmail.com>
2018-09-10 23:59:51 +0300

    WIP

--- joker-experiments/src/main/java/cs/bilkent/joker/experiment/LatencyTestMain.java
+++ joker-experiments/src/main/java/cs/bilkent/joker/experiment/LatencyTestMain.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
+import com.typesafe.config.Config;
+
 import static com.google.common.util.concurrent.Uninterruptibles.sleepUninterruptibly;
+import static com.typesafe.config.ConfigFactory.systemProperties;
 import cs.bilkent.joker.Joker;
 import cs.bilkent.joker.Joker.JokerBuilder;
 import cs.bilkent.joker.engine.config.JokerConfigBuilder;
@@ -29,6 +32,12 @@ public class LatencyTestMain
     private static final int KEY_RANGE = 10000;
 
     private static final int MULTIPLIER_VALUE = 100;
+
+    public static final String PRODUCED_TUPLE_COUNT_PER_SOURCE_INVOCATION = "producedTupleCountPerSourceInvocation";
+
+    public static final String MAPPER_OPERATOR_BATCH_SIZE = "mapperOperatorBatchSize";
+
+    public static final String MULTIPLICATION_COUNT = "multiplicationCount";
 
 
     static class ValueGenerator implements Consumer<Tuple>
@@ -79,16 +88,22 @@ public class LatencyTestMain
 
     public static void main ( String[] args )
     {
+        final Config config = systemProperties();
+        final int producedTupleCountPerSourceInvocation = config.getInt( PRODUCED_TUPLE_COUNT_PER_SOURCE_INVOCATION );
+        final int mapperOperatorBatchSize = config.getInt( MAPPER_OPERATOR_BATCH_SIZE );
+        final int multiplicationCount = config.getInt( MULTIPLICATION_COUNT );
+
         final ValueGenerator valueGenerator = new ValueGenerator( KEY_RANGE );
         final OperatorConfig beacon1Config = new OperatorConfig().set( TUPLE_POPULATOR_CONFIG_PARAMETER, valueGenerator )
-                                                                 .set( TUPLE_COUNT_CONFIG_PARAMETER, 1 );
+                                                                 .set( TUPLE_COUNT_CONFIG_PARAMETER,
+                                                                       producedTupleCountPerSourceInvocation );
 
         final OperatorDef beacon = OperatorDefBuilder.newInstance( "beacon", BeaconOperator.class ).setConfig( beacon1Config ).build();
 
         final OperatorConfig multiplierConfig = new OperatorConfig().set( MAPPER_CONFIG_PARAMETER,
                                                                           (BiConsumer<Tuple, Tuple>) ( input, output ) -> {
                                                                               int val = input.getInteger( "value" );
-                                                                              for ( int i = 0; i < 1; i++ )
+                                                                              for ( int i = 0; i < multiplicationCount; i++ )
                                                                               {
                                                                                   val = val * MULTIPLIER_VALUE - val;
                                                                               }
@@ -103,7 +118,7 @@ public class LatencyTestMain
         final FlowDef flow = new FlowDefBuilder().add( beacon ).add( multiplier ).connect( "beacon", "multiplier" ).build();
 
         final JokerConfigBuilder configBuilder = new JokerConfigBuilder();
-        configBuilder.getTupleQueueDrainerConfigBuilder().setMaxBatchSize( 1 );
+        configBuilder.getTupleQueueDrainerConfigBuilder().setMaxBatchSize( mapperOperatorBatchSize );
         configBuilder.getTupleQueueManagerConfigBuilder().setMultiThreadedQueueDrainLimit( 1 );
         configBuilder.getMetricManagerConfigBuilder().setTickMask( 3 );
         configBuilder.getMetricManagerConfigBuilder().setPipelineMetricsScanningPeriodInMillis( 1000 );
@@ -116,6 +131,8 @@ public class LatencyTestMain
         joker.run( flow );
 
         sleepUninterruptibly( 120, SECONDS );
+
+        joker.shutdown();
     }
 
 }
